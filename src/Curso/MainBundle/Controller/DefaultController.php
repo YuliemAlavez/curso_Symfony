fe7b5328b0cf2db69c1dd2050ece1097945ef1d4
Yuliem D. Alavez García <yuliemalavez@outlook.com>
2015-01-13 09:28:18 -0600

    Creación de una nueva página
    Se modifico el archivo routing para agregar una ruta a la nueva página.,
    se modificó el controlador default para agregar una acción y finalmente
    dentro de la acción default se creó una nueva página para la ayuda.

--- src/Curso/MainBundle/Controller/DefaultController.php
+++ src/Curso/MainBundle/Controller/DefaultController.php
@@ -3,11 +3,17 @@
 namespace Curso\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('CursoMainBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+    public function ayudaAction($tema)
+    {
+        return new Response("<html><body>Esta es la ayuda sobre el tema ".$tema."</body></html>");
     }
 }
